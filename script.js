5e4372ba7f5829837569f258d7233d820e6554a7
Shikhardev <shikhardev132@gmail.com>
2026-01-17 01:00:05 +0530

    Update script.js
    bhar mein jao

--- script.js
+++ script.js
@@ -5,20 +5,8 @@ function toggleMenu(){
   } else {
     menu.style.display = "block";
   }
-    const form = document.getElementById("contactForm");
-const msg = document.getElementById("thankYouMsg");
-
-form.addEventListener("submit", function (e) {
+  document.getElementById("contactForm").addEventListener("submit", function(e){
   e.preventDefault();
-
-  fetch(""https://script.google.com/macros/s/AKfycbzWoGO1Vg48Wqig5cFFoohoeQegDvL7d-v3wLtnOnojCMA6Ypf3J2SEeqnE68bJCKi6/exec"", {
-    method: "POST",
-    body: new FormData(form)
-  })
-  .then(() => {
-    form.reset();
-    msg.style.display = "block";
-    setTimeout(() => msg.style.display = "none", 5000);
-  })
-  .catch(() => alert("Something went wrong"));
+  alert("Thank you! We will contact you soon.");
+  this.reset();
 });
