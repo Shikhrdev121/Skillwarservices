379a2e40d3a2596db1034fa77dbd0564aa4bf587
Shikhardev <shikhardev132@gmail.com>
2026-01-17 00:41:15 +0530

    Update script.js
    Add form

--- script.js
+++ script.js
@@ -5,9 +5,24 @@ function toggleMenu(){
   } else {
     menu.style.display = "block";
   }
-document.getElementById("contactForm").addEventListener("submit", function(e){
-  e.preventDefault();
-  alert("Thank you! We will contact you soon.");
-  this.reset();
+  fetch("https://script.google.com/macros/s/AKfycbzWoGO1Vg48Wqig5cFFoohoeQegDvL7d-v3wLtnOnojCMA6Ypf3J2SEeqnE68bJCKi6/exec", {
+    method: "POST",
+    body: new FormData(form)
+  })
+  .then(() => {
+    form.reset();
+    thankYouMsg.style.display = "block";
+
+    // optional: hide after 5 seconds
+    setTimeout(() => {
+      thankYouMsg.style.display = "none";
+    }, 5000);
+  })
+  .catch(() => {
+    alert("❌ Something went wrong. Please try again.");
+  });
 });
+</script>
+
+
 
